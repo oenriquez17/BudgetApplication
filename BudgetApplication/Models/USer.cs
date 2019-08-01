40472c7de0d84b8016107ffe26653b8fd6c97c8d
Oscar Enriquez <oscar.enriquez.cs@gmail.com>
2019-07-31 23:04:55 -0400

    Added user model

---
+++ BudgetApplication/Models/USer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BudgetApplication.Models
+{
+    public class User
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
