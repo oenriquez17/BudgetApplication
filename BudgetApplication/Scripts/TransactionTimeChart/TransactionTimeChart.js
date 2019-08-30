225eb9f13a531712753b592df15c32db1b6997a8
Oscar Enriquez <oscar.enriquez.cs@gmail.com>
2019-08-30 16:45:03 -0400

    Added a function to parse DateTypes

--- BudgetApplication/Scripts/TransactionTimeChart/TransactionTimeChart.js
+++ BudgetApplication/Scripts/TransactionTimeChart/TransactionTimeChart.js
@@ -1,1 +1,10 @@
-﻿
+﻿for (var i = 0; i < transactions.length; i++) {
+    console.log(ToJavaScriptDate(transactions[i].DateOfTransaction));
+}
+
+function ToJavaScriptDate(value) {
+    var pattern = /Date\(([^)]+)\)/;
+    var results = pattern.exec(value);
+    var dt = new Date(parseFloat(results[1]));
+    return (dt.getMonth() + 1) + "/" + dt.getDate() + "/" + dt.getFullYear();
+}
